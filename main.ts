599e7623fa0583bb69ebac30fd492846ca8f4c1b
Mohammad Wasi <150406862+MohammadWasi12345@users.noreply.github.com>
2024-01-30 04:30:15 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,72 @@
+import inquirer from "inquirer";
+
+let loop = true;
+
+interface Currencyconvertor{
+    currencyInput:string,
+    amount:number,
+    currencyOutput:string
+}
+
+while(loop){
+ const currencyConvertor:Currencyconvertor =await inquirer.prompt([{
+    type:'list',
+    name:'currencyInput',
+    message:'Select currency you want to convert',
+    choices:['pakistani rupee','saudi riyal','US dollar']
+
+
+ },
+ {
+    type:'number',
+    name: 'amount',
+    message:'enter your amout'
+ },
+ {
+    type:'list',
+    name: 'currencyOutput',
+    message:'Select currncy you want ',
+    choices:['pakistani rupee','saudi riyal','US dollar']
+
+ }
+]);
+
+const input = currencyConvertor.currencyInput;
+const money = currencyConvertor.amount;
+const output =currencyConvertor.currencyOutput;
+
+if(input === 'pakistani rupee'){
+    if(output === 'US dollar'){
+        console.log(`The ${money}Rs is equal to ${money/281}$. `)
+    }else if(output==='saudi riyal'){
+        console.log(`The ${money}Rs is equal to ${money/75}riyal.`)
+    }
+
+}else if(input === 'US dollar'){
+    if(output === 'pakistani rupee'){
+        console.log(`The ${money}$ is equal to ${money*281}Rs. `)
+    }else if(output==='saudi riyal'){
+        console.log(`The ${money}$ is equal to ${money*3.75}riyal.`)
+    }
+
+}else if(input === 'saudi riyal'){
+    if(output === 'US dollar'){
+        console.log(`The ${money}riyal is equal to ${money/3.75}$. `)
+    }else if(output==='pakistani rupee'){
+        console.log(`The ${money}riyal is equal to ${money*75}Rs.`)
+    }
+
+}
+
+const calculateAgain =await inquirer.prompt({
+    type:'confirm',
+    name: 'calculateMore',
+    message:'do you want more conversion?',
+    default:false
+});
+if(!calculateAgain.calculateMore){
+    loop=false;
+}
+
+
+};
